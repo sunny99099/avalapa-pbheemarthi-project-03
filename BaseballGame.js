9d2b73c7cb43ab6e9afb85fa124b7f2da08da741
avalapa <avalapa@uco.edu>
2024-12-06 21:19:25 -0600

    100%

--- BaseballGame.js
+++ BaseballGame.js
@@ -5,14 +5,12 @@ class BaseballGame {
         this.currentGuess = [];
     }
 
-    // Generate a random secret key with unique digits
     generateSecretKey() {
         const digits = Array.from({ length: 10 }, (_, i) => i); // [0-9]
         digits.sort(() => Math.random() - 0.5); // Shuffle
         this.secretKey = digits.slice(0, this.maxDigits);
     }
 
-    // Check the user's guess against the secret key
     checkGuess() {
         let strikes = 0;
         let balls = 0;
@@ -30,13 +28,11 @@ class BaseballGame {
         return { strikes, balls };
     }
 
-    // Reset the game state for a new game
     newGame() {
         this.generateSecretKey();
         this.currentGuess = [];
     }
 
-    // Update the current guess and return true if it's complete
     addToGuess(digit) {
         if (this.currentGuess.length < this.maxDigits && !this.currentGuess.includes(digit)) {
             this.currentGuess.push(digit);
